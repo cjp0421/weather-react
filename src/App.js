14e3ce5b9bff4d9122f07e829dd83ccdb920f570
cjp0421 <pedersencarol1@gmail.com>
2022-02-28 15:41:01 -0600

    updated page title

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <h1>Hello Weather</h1>
       </header>
       <Search />
     </div>

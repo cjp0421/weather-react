36a25871cbe6241ba65593a4609a225be661232b
cjp0421 <pedersencarol1@gmail.com>
2022-04-04 10:33:51 -0500

    changed number of forecast days from 6 to 5

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -18,7 +18,7 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
-            if (index < 6) {
+            if (index < 5) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay forecast={dailyForecast} />

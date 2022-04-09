6529ebc3cccf0aaa6af9d6204633aad49f2ca891
cjp0421 <pedersencarol1@gmail.com>
2022-04-09 15:01:19 -0500

    updated jsx and css
    fixed spacing issues including odd padding problems

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App container">
+    <div className="App">
       <header className="App-header">
         <h1>Hello Weather</h1>
       </header>
